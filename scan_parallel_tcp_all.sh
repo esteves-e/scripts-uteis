5ff003ab123502f10d1bbc8445c8e782ddf06bb2
esteves-e <32077947+esteves-e@users.noreply.github.com>
2025-07-17 14:02:23 -0300

    Create scan_parallel_tcp_all.sh

---
+++ scan_parallel_tcp_all.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+IP_LIST="ips.txt"
+LIVE_HOSTS="hosts_ativos.txt"
+OUTPUT_DIR="resultados"
+mkdir -p "$OUTPUT_DIR"
+
+echo "[*] Verificando hosts ativos..."
+nmap -sn -iL "$IP_LIST" -oG - | awk '/Up$/{print $2}' > "$LIVE_HOSTS"
+
+echo "[*] Varredura em paralelo nos hosts ativos..."
+
+cat "$LIVE_HOSTS" | parallel -j 10 "nmap -Pn -T4 -p- {} -oN $OUTPUT_DIR/{}.txt"
+
+echo "[*] Varredura concluída. Resultados estão em $OUTPUT_DIR/"
